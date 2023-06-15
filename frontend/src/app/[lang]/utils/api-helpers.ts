70e7691d0994912f36ec4d2bb0e9d0de0789a607
Marktawa <markmunyaka@gmail.com>
2023-06-15 20:50:57 +0000

    Added API helper functions

---
+++ frontend/src/app/[lang]/utils/api-helpers.ts
@@ -0,0 +1,25 @@
+// ./frontend/src/app/[lang]/utils/api-helpers.ts
+
+export function getStrapiURL(path = '') {
+    return `${process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'}${path}`;
+}
+
+export function getStrapiMedia(url: string | null) {
+    if (url == null) {
+        return null;
+    }
+
+    // Return the full URL if the media is hosted on an external provider
+    if (url.startsWith('http') || url.startsWith('//')) {
+        return url;
+    }
+
+    // Otherwise prepend the URL path with the Strapi URL
+    return `${getStrapiURL()}${url}`;
+}
+
+export function formatDate(dateString: string) {
+    const date = new Date(dateString);
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+}
